505bf8bebd8da9489ce73347f5d7e0137dcccbfc
justin <justin@sharktub.v2>
2025-06-18 21:10:17 -0500

    create a for loop to iterate over the pokemon to display

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,26 @@
 const pokemonList = [
   {
     name: "Caterpie",
-    height: 3,
+    height: 0.3,
     types: ["bug"],
   },
   {
     name: "Charmander",
-    height: 6,
+    height: 0.6,
     types: ["fire"],
   },
   {
     name: "Swinub",
-    height: 4,
+    height: 0.4,
     types: ["ice", "ground"],
   },
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  // this conditional that checks the pokemon's height lives within the document.write argument for the sake of being succinct
+  document.write(
+    pokemonList[i].height > 0.5
+      ? `${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow, that's big!</br>`
+      : `${pokemonList[i].name} (height: ${pokemonList[i].height})</br>`,
+  );
+}

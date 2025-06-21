8667d7957002514ec436bd6a9f9718e0080d2561
justin <justin@sharktub.v2>
2025-06-20 22:24:43 -0500

    refactor scripts.js using IIFE and add methods for add, getAll, and filter

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,51 @@
-const pokemonList = [
-  {
-    name: "Caterpie",
-    height: 0.3,
-    types: ["bug"],
-  },
-  {
-    name: "Charmander",
-    height: 0.6,
-    types: ["fire"],
-  },
-  {
-    name: "Swinub",
-    height: 0.4,
-    types: ["ice", "ground"],
-  },
-];
+const pokemonRepository = (function () {
+  const pokemonList = [
+    {
+      name: "Caterpie",
+      height: 0.3,
+      types: ["bug"],
+    },
+    {
+      name: "Charmander",
+      height: 0.6,
+      types: ["fire"],
+    },
+    {
+      name: "Swinub",
+      height: 0.4,
+      types: ["ice", "ground"],
+    },
+  ];
 
-for (let i = 0; i < pokemonList.length; i++) {
+  function getAll() {
+    return pokemonList;
+  }
+
+  function add(pokemon) {
+    if (typeof pokemon !== "object") {
+      return "Incorrect data type";
+    }
+    pokemonList.push(pokemon);
+  }
+
+  function filter(name) {
+    return pokemonList.filter((pokemon) => {
+      return pokemon.name === name;
+    });
+  }
+
+  return {
+    getAll: getAll,
+    add: add,
+    filter: filter,
+  };
+})();
+
+pokemonRepository.getAll().forEach((pokemon) => {
   // this conditional that checks the pokemon's height lives within the document.write argument for the sake of being succinct
   document.write(
-    pokemonList[i].height > 0.5
-      ? `${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow, that's big!</br>`
-      : `${pokemonList[i].name} (height: ${pokemonList[i].height})</br>`,
+    pokemon.height > 0.5
+      ? `${pokemon.name} (height: ${pokemon.height}) - Wow, that's big!</br>`
+      : `${pokemon.name} (height: ${pokemon.height})</br>`,
   );
-}
+});
